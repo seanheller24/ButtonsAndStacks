c64c0f36bfd0a3394e8eac6543c2faa61fe9112e
seanheller24 <hellerse@bc.edu>
2022-01-31 14:45:24 -0500

    2.12 partial

--- ButtonsAndStacks/ViewController.swift
+++ ButtonsAndStacks/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var colorLabel: UILabel!
+    
+    var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .systemPurple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func colorButtonPressed(_ sender: UIButton) {
+        print("You clicked tag #:\(sender.tag)")
+        print(sender.currentTitle!)
+    }
+    
+    
 }
 
